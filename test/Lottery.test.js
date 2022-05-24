a2228913262fd03d6aa8a9313cf1168ca3cf9ba9
AryanKuAg <alemantrixaryan@gmail.com>
2022-05-25 01:12:38 +0530

    requires a minimum amount of enter to enter added

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -18,4 +18,31 @@ describe('describe test', () => {
     it('simple unit it test', () => {
         assert.ok(lottery.options.address);
     })
+
+    it('allows one account to enter', async() => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', 'ether')
+        })
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        })
+
+        assert.equal(accounts[0], players[0])
+        assert.equal(1, players.length)
+    } )
+
+    it('requires a minimum amount of ether to enter', async() => {
+        try{
+            await lottery.methods.enter().send({
+                from: accounts[0],
+                value: 0  // in wei
+            })
+
+            assert(false);
+        }catch(e){
+            assert(e);
+        }
+    })
 })
